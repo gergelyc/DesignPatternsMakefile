82fb906b17c76bbaa4e6637103731ceea5d196f8
Gergely Csomos <csomos.gergo@gmail.com>
2015-12-28 14:28:47 +0100

    Add Wrapper template clas

---
+++ include/Wrapper.h
@@ -0,0 +1,68 @@
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+template<class T>
+class Wrapper
+{
+public:
+	Wrapper() : DataPtr{ nullptr }
+	{
+	}
+
+	Wrapper(const T& inner)
+	{
+		DataPtr = inner.clone();
+	}
+
+	Wrapper(const Wrapper<T>& original)
+	{
+		if (original.DataPtr)
+			DataPtr = original.DataPtr->clone();
+		else
+			DataPtr = nullptr;
+	}
+
+	~Wrapper()
+	{
+		delete DataPtr;
+	}
+
+	Wrapper& operator=(const Wrapper<T>& original)
+	{
+		if (this != original)
+		{
+			if (DataPtr)
+				delete DataPtr;
+			if (original.DataPtr)
+				DataPtr = original.DataPtr->clone();
+			else
+				DataPtr = nullptr;
+		}
+		return *this;
+	}
+
+	T& operator*()
+	{
+		return *DataPtr;
+	}
+
+	const T& operator*() const
+	{
+		return *DataPtr;
+	}
+
+	T* operator->()
+	{
+		return DataPtr;
+	}
+
+	const T* operator->() const
+	{
+		return DataPtr;
+	}
+
+private:
+	T* DataPtr;
+};
+
+#endif
