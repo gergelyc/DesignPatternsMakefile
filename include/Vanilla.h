0e495c0c56d7b128065ac0226a376210b2a3d9f3
Gergely Csomos <csomos.gergo@gmail.com>
2015-12-28 09:39:24 +0100

    Add VanillaOption class

---
+++ include/Vanilla.h
@@ -0,0 +1,26 @@
+#ifndef VANILLA_H
+#define VANILLA_H
+#include "PayOff.h"
+
+class VanillaOption
+{
+public:
+	VanillaOption(PayOff& ThePayOff_, double Expiry_);
+	VanillaOption(const VanillaOption& original);
+	VanillaOption& operator=(const VanillaOption& original);
+	double GetExpiry() const
+	{
+		return Expiry;
+	}
+	double OptionPayOff(double Spot) const
+	{
+		return (*ThePayOffPtr)(Spot);
+	}
+	~VanillaOption();
+
+private:
+	double Expiry;
+	PayOff* ThePayOffPtr;
+};
+
+#endif
